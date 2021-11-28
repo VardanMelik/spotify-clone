e9ba7afcb0837a4bfb2c817e7f364fe98b98ddcd
EYE MOTION <eyemotiongamestudio@gmail.com>
2021-11-28 21:37:57 +0400

    update

---
+++ pages/index.js
@@ -0,0 +1,19 @@
+import Head from 'next/head'
+import Sidebar from '../components/Sidebar'
+
+export default function Home() {
+  return (
+    <div className="">
+      
+      <main>
+        <Sidebar />
+        {/* Center */}
+      </main>
+
+      <div>
+        {/* Player */}
+      </div>
+     
+    </div>
+  )
+}
---
+++ timer.js
@@ -0,0 +1,8 @@
+/*
+
+
+15:36/4:01:19
+https://www.youtube.com/watch?v=3xrko3GpYoU
+
+
+*/
